3d0d5c972a4a2a787d24776c5679ea163c25e081
unknown <8x15yz@gmail.com>
2025-03-20 08:25:52 +0900

    .

--- 02-django/project/mongo_driver.py
+++ 02-django/project/mongo_driver.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 ### 배포시 변경
-uri= "mongodb://bluemap.kr:21801/"
-# uri= "mongodb://bluemap.kr:21805/"
+# uri= "mongodb://bluemap.kr:21801/"
+uri= "mongodb://bluemap.kr:21805/"
 # uri= "mongodb://localhost:27017/"
 client = MongoClient(uri)
 
--- 03-react/front-end/src/deployUrl.js
+++ 03-react/front-end/src/deployUrl.js
@@ -1,3 +1,3 @@
 /// 배포시 변경
-export const ROOT_DEPLOY_URL = 'http://bluemap.kr:21803';
-// export const ROOT_DEPLOY_URL = 'http://127.0.0.1:8000';
+// export const ROOT_DEPLOY_URL = 'http://bluemap.kr:21803';
+export const ROOT_DEPLOY_URL = 'http://127.0.0.1:8000';
